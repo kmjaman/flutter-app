14ff05bd4f009023d19e1403feaf783fb917c5d3
Jaman <jaman.edux@gmail.com>
2021-08-05 01:05:51 +0200

    added button

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,38 @@ void main() => runApp(MyApp());
 
 
 class MyApp extends StatelessWidget {
+
+  void answerQuestion(){
+    print('Answer Chosen!');
+  }
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\s your favourite color',
+      'What\s your favourite animal',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
           ),
-          body: Text('This is my homepage!'),
+          body: Column(
+            children: [
+              Text('The question!'),
+              ElevatedButton(
+                child: Text('Answer 1'),
+                onPressed: answerQuestion,
+              ),
+              ElevatedButton(
+                child: Text('Answer 2'),
+                onPressed: answerQuestion,
+              ),
+              ElevatedButton(
+                child: Text('Answer 3'),
+                onPressed: answerQuestion,
+              ),
+            ],
+          ),
       ),
     );
   }
