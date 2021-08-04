126ac459a0639fed0b0eb9dfec1f53822962cfad
Jaman <jaman.edux@gmail.com>
2021-08-05 00:05:59 +0200

    changed homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
+
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'),);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+          ),
+          body: Text('This is my homepage!'),
+      ),
+    );
   }
 }
