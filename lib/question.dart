8bcaa69af99edf680815cd4947460c8737f9b0b9
Jaman <jaman.edux@gmail.com>
2021-08-07 00:21:22 +0200

    added margin

--- lib/question.dart
+++ lib/question.dart
@@ -7,6 +7,6 @@ class Question extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(width: double.infinity, child: Text(questionText, style: TextStyle(fontSize: 28), textAlign: TextAlign.center,),);
+    return Container(width: double.infinity, margin: EdgeInsets.all(50), child: Text(questionText, style: TextStyle(fontSize: 28), textAlign: TextAlign.center,),);
   }
 }
